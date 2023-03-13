99ada8f87bb0a20739ba46b3288d98e2da31b6c8
overshiki <le.niu@hotmail.com>
2023-03-13 23:23:28 +0800

    more complex typeclass

--- Symbolic.hs
+++ Symbolic.hs
@@ -1,13 +1,15 @@
 module Symbolic () where 
 
 import NaiveTensor.NTensor
+import NaiveTensor.Broadcast
 import NaiveTensor.Matrix
 import NaiveTensor.Utils
 import NumericalDist
 import GHC.Float (int2Float)
 
 -- mean vector 
-type Mean = [Float]
+data Pos a = Pos [a] deriving (Show, Eq)
+type Mean = Pos Float
 
 -- covariance matrix 
 type CoVar = NaiveTensor Float
@@ -15,24 +17,36 @@ data Gaussian = Gaussian Mean CoVar deriving (Show, Eq)
 
 -- drop the first dimention, i.e. to the right 
 instance Marginal Gaussian where
-    marginal (Gaussian (x:xs) covar) = Gaussian xs (tailMatrix covar)
+    marginal (Gaussian (Pos (x:xs)) covar) = Gaussian (Pos xs) (tailMatrix covar)
+
+class ListUnpackable un where
+    unpack :: un a -> [a]
+
+instance ListUnpackable Pos where
+    unpack (Pos x) = x
+
 
 -- by default, to the right 
-instance Conditional Gaussian where 
-    conditional data (Gaussian mean covar) 
-            = 
+-- instance Conditional Gaussian where 
+--     conditional data (Gaussian mean covar) 
+--             = 
 
-                where 
-                    l = length data
-                    [ori_mean, cond_mean] = listSplitAt l mean
-                    [lu,ld,ru,rd] = matrixSplitAt (l,l) covar 
-                    
+--                 where 
+--                     l = length data
+--                     [ori_mean, cond_mean] = listSplitAt l mean
+--                     [lu,ld,ru,rd] = matrixSplitAt (l,l) covar 
 
 
-(.-) :: [Float] -> [Float] -> [Float]
-(.-) xs ys = map func (zip xs ys)
-        where 
-            func (x, y) = x - y
+
+-- (.-) :: [Float] -> [Float] -> [Float]
+tuple_apply :: (a -> b -> c) -> (a, b) -> c 
+tuple_apply func (x, y) = func x y
+instance (Fractional a) => BroadcastableNum (Pos a) where
+    (.+) (Pos xs) (Pos ys) = Pos $ map (tuple_apply (+)) (zip xs ys)
+    (.-) (Pos xs) (Pos ys) = Pos $ map (tuple_apply (-)) (zip xs ys)
+    (.*) (Pos xs) (Pos ys) = Pos $ map (tuple_apply (*)) (zip xs ys)
+    (./) (Pos xs) (Pos ys) = Pos $ map (tuple_apply (/)) (zip xs ys)
+
 
 -- TODO: implement determinate 
 gaussian_pdf :: Gaussian -> [Float] -> Float 
@@ -42,9 +56,9 @@ gaussian_pdf (Gaussian mean covar) x =
                 squeeze x = (flatten2list x) !! 0
                 -- temperory setting determinate to 1.0, implementing the real one in the future 
                 determinate = 1.0
-                diff = list2flatten $ x .- mean 
+                diff = list2flatten $ unpack $ (Pos x) .- mean 
                 ee = -0.5 * (squeeze ((diff @@ (inv covar)) @@ diff))
-                l = int2Float (length mean)
+                l = int2Float (length $ unpack mean)
                 z = (2 * pi)**(l/2)
 
 instance Pdf Gaussian where 
@@ -55,11 +69,11 @@ instance Pdf Gaussian where
 
 main :: IO ()
 main = do 
-    let x = [1..5]
-        y = [2..6]
+    let x = Pos [1..5]
+        y = Pos [2..6]
     print $ x .- y
 
-    let mean = [0.0, 0.0]
+    let mean = Pos [0.0, 0.0]
         covar = fromList [2,2] [1,0,0,1]
         gg = Gaussian mean covar
     print $ pdf gg [1.0,1.0]
